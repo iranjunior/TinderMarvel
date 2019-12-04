b98eaf04fb0f4b55ea57d7eb7b52db38a489ce91
“Iran <iranjunior94@gmail.com>
2019-12-04 07:39:05 -0300

    gesture okay

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -18,38 +18,64 @@ import {
 
 const Main = ({navigation}) => {
   const translateY = new Animated.Value(0);
+  const translateX = new Animated.Value(0);
+  const rotate = translateX.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ['20deg', '-20deg'],
+  });
+
+  /*  Animated.timing(translateY, {
+    toValue: 100,
+    duration: 4000,
+    useNativeDriver: true,
+  }).start();
+ */
   const animatedEvent = Animated.event(
     [
       {
         nativeEvent: {
           translationY: translateY,
+          translationX: translateX,
         },
       },
     ],
     {useNativeDriver: true},
   );
-
   return (
     <PanelBackground>
-      <PanGestureHandler onGestureEvent={animatedEvent}>
-        <Card
-          style={{
-            transform: [
-              {
-                translateY: translateY,
-              },
-            ],
-          }}>
-          <ImageProfile source={require('~/public/imagens/500.jpg')} />
-          <TextArea type="Title">
-            <Title> Homem de Ferro</Title>
-            <Description>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry...
-            </Description>
-          </TextArea>
-        </Card>
+      <PanGestureHandler
+        onGestureEvent={animatedEvent}
+        onHandlerStateChange={event => {}}>
+        <Animated.View
+          style={[
+            {
+              height: 100,
+              width: 100,
+              backgroundColor: 'green',
+            },
+            {
+              transform: [
+                {
+                  translateY: translateY,
+                },
+                {
+                  rotate: rotate,
+                },
+              ],
+            },
+          ]}
+        />
       </PanGestureHandler>
+      {/*  <Card>
+        <ImageProfile source={require('~/public/imagens/500.jpg')} />
+        <TextArea type="Title">
+          <Title> Homem de Ferro</Title>
+          <Description>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry...
+          </Description>
+        </TextArea>
+      </Card>
       <ViewMain type="Footer">
         <Bottom>
           <Icon type="Unlike">✖</Icon>
@@ -57,7 +83,7 @@ const Main = ({navigation}) => {
         <Bottom>
           <Icon type="Like">✔</Icon>
         </Bottom>
-      </ViewMain>
+      </ViewMain> */}
     </PanelBackground>
   );
 };
